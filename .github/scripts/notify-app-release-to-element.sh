22a3aa982511e41aba2e2a2a3ec006e3849cd877
nabim777 <nabinalemagar019@gmail.com>
2025-11-05 14:24:07 +0545

    for send message making a method
    Signed-off-by: nabim777 <nabinalemagar019@gmail.com>

--- .github/scripts/notify-app-release-to-element.sh
+++ .github/scripts/notify-app-release-to-element.sh
@@ -58,30 +58,8 @@ get_latest_release_tag(){
   fi
 }
 
-
-log_info "Fetching latest Nextcloud release tag....."
-
-
-# ELEMENT_CHAT_URL=https://matrix.openproject.org
-# ELEMENT_ROOM_ID=wNGZBbAPrhCiGXtQYp:openproject.org
-# REPO_OWNER= nextcloud
-# RUN_ID=123456789
-# LATEST_SUPPORTED_NC_VERSION=31
-
-
-# nextcloud_latest_release_tag=$(curl -s -H "Authorization: token $TOKEN_GITHUB" \
-# "https://api.github.com/repos/nextcloud/server/releases" | jq -r '.[] | select(.prerelease == false) | .tag_name' | head -n 1)
-
-
-get_latest_release_tag "server"
-
-
-
-# major_version=$(echo "$nextcloud_latest_release_tag" | sed -E 's/^v([0-9]+)\..*/\1/')
-
-# log_info "Latest release tag: $major_version"
-
-if dpkg --compare-versions "$nextcloud_latest_release_tag" gt "$LATEST_SUPPORTED_NC_VERSION"; then
+send_message_to_room(){
+  if dpkg --compare-versions "$nextcloud_latest_release_tag" gt "$LATEST_SUPPORTED_NC_VERSION"; then
     log_info "New Nextcloud release detected: $nextcloud_latest_release_tag"
     send_message_to_room_response=$(curl -s -XPOST "$ELEMENT_CHAT_URL/_matrix/client/r0/rooms/%21$ELEMENT_ROOM_ID/send/m.room.message?access_token=$NIGHTLY_CI_USER_TOKEN" \
                                     -d '
@@ -102,7 +80,33 @@ if dpkg --compare-versions "$nextcloud_latest_release_tag" gt "$LATEST_SUPPORTED
         log_error "Response: $send_message_to_room_response"
         exit 1
     fi
-else
-    log_info "No new Nextcloud release. Latest stable version is still: $LATEST_SUPPORTED_NC_VERSION"
-    exit 0
-fi
+  else
+      log_info "No new Nextcloud release. Latest stable version is still: $LATEST_SUPPORTED_NC_VERSION"
+      exit 0
+  fi
+}
+
+
+log_info "Fetching latest Nextcloud release tag....."
+
+
+# ELEMENT_CHAT_URL=https://matrix.openproject.org
+# ELEMENT_ROOM_ID=wNGZBbAPrhCiGXtQYp:openproject.org
+# REPO_OWNER= nextcloud
+# RUN_ID=123456789
+# LATEST_SUPPORTED_NC_VERSION=31
+
+
+# nextcloud_latest_release_tag=$(curl -s -H "Authorization: token $TOKEN_GITHUB" \
+# "https://api.github.com/repos/nextcloud/server/releases" | jq -r '.[] | select(.prerelease == false) | .tag_name' | head -n 1)
+
+
+get_latest_release_tag "server"
+
+send_message_to_room
+
+
+
+# major_version=$(echo "$nextcloud_latest_release_tag" | sed -E 's/^v([0-9]+)\..*/\1/')
+
+# log_info "Latest release tag: $major_version"
