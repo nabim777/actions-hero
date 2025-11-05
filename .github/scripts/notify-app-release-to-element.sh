344787af8418da33a9b9fea4174fe1e314a6c9fe
nabim777 <nabinalemagar019@gmail.com>
2025-11-05 12:58:08 +0545

    use dpkg to compare the version
    Signed-off-by: nabim777 <nabinalemagar019@gmail.com>

--- .github/scripts/notify-app-release-to-element.sh
+++ .github/scripts/notify-app-release-to-element.sh
@@ -21,7 +21,7 @@ update_repo_variable(){
         return 1
     fi
 
-    new_value=$((LATEST_SUPPORTED_NC_VERSION + 1));
+    new_value=$nextcloud_latest_release_tag;
     
     log_info "Updating variable '$variable_name' to value '$new_value'..."
     
@@ -43,8 +43,14 @@ update_repo_variable(){
 
 get_latest_release_tag(){
   repo_name=$1
-  nextcloud_latest_release_tag=$(curl -s -H "Authorization: token $TOKEN_GITHUB" \
-"https://api.github.com/repos/nextcloud/$repo_name/releases" | jq -r '.[] | select(.prerelease == false) | .tag_name' | head -n 1)
+  releases_json=$(curl -s -H "Authorization: token $TOKEN_GITHUB" "https://api.github.com/repos/nextcloud/$repo_name/releases")
+
+  # extract the latest stable release tag by ignoring pre-releases
+  nextcloud_latest_release_tag=$(echo "$releases_json" | jq -r '.[] | select(.prerelease == false) | .tag_name' | head -n 1)
+
+  # Remove leading 'v' if present
+  nextcloud_latest_release_tag=${nextcloud_latest_release_tag#v}
+
   # Check if the tag is empty or null
   if [[ -z "$nextcloud_latest_release_tag" || "$nextcloud_latest_release_tag" == "null" ]]; then
       log_error "Failed to fetch Nextcloud latest release tag. Tag is empty or null."
@@ -71,12 +77,11 @@ get_latest_release_tag "server"
 
 
 
-major_version=$(echo "$nextcloud_latest_release_tag" | sed -E 's/^v([0-9]+)\..*/\1/')
+# major_version=$(echo "$nextcloud_latest_release_tag" | sed -E 's/^v([0-9]+)\..*/\1/')
 
-log_info "Latest release tag: $major_version"
+# log_info "Latest release tag: $major_version"
 
-
-if [[ "$major_version" -gt "$LATEST_SUPPORTED_NC_VERSION" ]]; then
+if dpkg --compare-versions "$nextcloud_latest_release_tag" gt "$LATEST_SUPPORTED_NC_VERSION"; then
     log_info "New Nextcloud release detected: $nextcloud_latest_release_tag"
     send_message_to_room_response=$(curl -s -XPOST "$ELEMENT_CHAT_URL/_matrix/client/r0/rooms/%21$ELEMENT_ROOM_ID/send/m.room.message?access_token=$NIGHTLY_CI_USER_TOKEN" \
                                     -d '
@@ -84,7 +89,7 @@ if [[ "$major_version" -gt "$LATEST_SUPPORTED_NC_VERSION" ]]; then
                                         "msgtype": "m.text",
                                         "body": "",
                                         "format": "org.matrix.custom.html",
-                                        "formatted_body": "<h3>🔔 New Nextcloud '$major_version' Release Alert!</h3><br><Run id = '$RUN_ID'>"
+                                        "formatted_body": "<h3>🔔 New Nextcloud '$nextcloud_latest_release_tag' Release Alert!</h3><br><Run id = '$RUN_ID'>"
                                         }
                                     '
                                     )
